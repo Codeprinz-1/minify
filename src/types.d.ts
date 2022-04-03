120739d40adc01383de65f256d8b9d86c33121a3
Codeprinz-1 <anyaohaprince122@gmail.com>
2022-04-03 21:23:34 +0100

    made fields in interface optional

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,9 @@
 interface Link {
-  _id: string;
-  slug: string;
-  secret: string;
+  _id?: string;
+  slug?: string;
+  secret?: string;
   url?: string;
   daysToLive?: number;
-  createDate: number;
-  timeToLive: number;
+  createDate?: number;
+  timeToLive?: number;
 }

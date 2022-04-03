5d986be33b2b3882caf91e442efb49420cbb60c8
Codeprinz-1 <anyaohaprince122@gmail.com>
2022-04-03 21:27:39 +0100

    added test links

--- src/tests/fixtures/db.ts
+++ src/tests/fixtures/db.ts
@@ -1,3 +1,20 @@
-const testLInk : Link = {
-    ur
-}
+const id1: string = "shgeiutongipstoietiyuti";
+const id2: string = "eorieopnldsoiyetngosytiy";
+const secret1: string = "teiuyont";
+const secret2: string = "shtoeiyth";
+
+const testLInk1: Link = {
+  _id: "sityentieytdityeoty",
+  url: "www.google.com",
+  secret: secret1,
+  createDate: Date.now(),
+  daysToLive: -1,
+};
+
+const testLink2: Link = {
+  _id: "eityoendoiteoistheiypeui",
+  url: "www.google.com",
+  secret: secret2,
+  createDate: Date.now(),
+  daysToLive: 1,
+};

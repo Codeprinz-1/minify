c7930b3ede3ad7141126d0dae9b0a83168b6f6b8
Codeprinz-1 <anyaohaprince122@gmail.com>
2022-04-04 19:44:27 +0100

    added get date difference function

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export const getDatDifference = (date1: string, date2: string): number => {
+  const dateObject1 = new Date(date1) as unknown as number;
+  const dateObject2 = new Date(date2) as unknown as number;
+  const diffTime = Math.abs(dateObject1 - dateObject2);
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+};

0eb60c42312c2dd58b4b07c05c0874ec33b84563
Codeprinz-1 <anyaohaprince122@gmail.com>
2022-04-05 14:10:35 +0100

    completed app

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import hbs from "hbs";
+import "./db/mongoose";
+import path from "path";
+import express from "express";
+import linkRouter from "./routers/link";
+
+const app = express();
+const port = process.env.PORT || 3003;
+
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.json());
+app.use(linkRouter);
+
+export default app;

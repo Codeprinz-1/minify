b39d3342a223f7907742d7b3cd34248bae2e5220
Codeprinz-1 <anyaohaprince122@gmail.com>
2022-05-07 17:35:07 +0100

    completed create resource controller

--- src/controllers/link.ts
+++ src/controllers/link.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import Link from "../models/link";
 
 export const homepageController = (
   req: express.Request,
@@ -17,7 +18,31 @@ export const redirectController = (
   res: express.Response
 ) => {};
 
-export const createController = (
+export const createController = async (
   req: express.Request,
   res: express.Response
-) => {};
+) => {
+  let slug;
+  if (req.body.slug) {
+    const link: LinkType | null = await Link.findById(req.body.slug);
+    if (link) {
+      res.status(400).send("slug is already taken, please choose another one");
+    } else {
+      slug = req.body.slug;
+    }
+  } else {
+    slug = generateSlug();
+    while (await Link.findById(slug)) {
+      slug = generateSlug();
+    }
+  }
+
+  const link = new Link({
+    url: req.body.url,
+    slug,
+    createdAt: new Date(),
+    secret: req.body.secret,
+  });
+  await link.save();
+  res.status(201).send({ slug });
+};

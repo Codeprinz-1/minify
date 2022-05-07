8f2421245178cd21fa16180b09a9875b478e6313
Codeprinz-1 <anyaohaprince122@gmail.com>
2022-05-07 16:31:37 +0100

    adde field created at to link model

--- src/models/link.ts
+++ src/models/link.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const LinkSchema = new mongoose.Schema<LinkType>({
-  createDate: { type: String, required: true },
+  createdAt: { type: String, required: true },
   expiryDate: { type: String, required: true },
   slug: { type: String, required: true },
   url: { type: String, required: true },
